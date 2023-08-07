08d73d6237d9d8427d1774813219ea6f8971ca85
Talha Türk <talhaturk@Talha-MacBook-Air.local>
2023-08-07 11:11:56 +0300

    first commit

---
+++ StoreApp/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Entities.Models;
+using Repositories;
+using Repositories.Contracts;
+
+namespace StoreApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IRepositoryManager _manager;
+
+        public ProductController(IRepositoryManager manager)
+        {
+            _manager = manager;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            var model = _manager.Product.GetAllProducts(false);
+            return View(model);
+        }
+
+        public IActionResult Get(int id)
+        {
+            var model = _manager.Product.GetOneProduct(id, false);
+            return View(model);
+        }
+    }
+}
---
+++ StoreApp/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using Repositories;
+using Repositories.Contracts;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<RepositoryContext>(options => 
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"),
+    b => b.MigrationsAssembly("StoreApp"));
+});
+
+builder.Services.AddScoped<IRepositoryManager,RepositoryManager>();
+builder.Services.AddScoped<IProductRepository,ProductRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+var app = builder.Build();
+
+
+app.UseStaticFiles();
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name:"default",
+    pattern:"{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
