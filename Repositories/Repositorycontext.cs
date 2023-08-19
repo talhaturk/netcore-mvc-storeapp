8c070e9143e969f5c5d17c5c5e8a5192e533e803
talhaturk <talhaturkk@icloud.com>
2023-08-19 21:42:59 +0300

    Oturum yonetimi ve orders  eklendi

--- Repositories/Repositorycontext.cs
+++ Repositories/Repositorycontext.cs
@@ -8,6 +8,7 @@ public class RepositoryContext : DbContext
 {
     public DbSet<Product> Products { get; set; }
     public DbSet<Category> Categories { get; set; }
+    public DbSet<Order> Orders { get; set; }
         public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options)
         {
             
